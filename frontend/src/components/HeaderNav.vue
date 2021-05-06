74ccdfe789d2fb5d525829c89b510f8fdaaf23cc
Guillaume Marteau <guillaumemarteau@hotmail.fr>
2021-05-06 19:26:57 +0200

    front: ajout plugins router, vuex et bootstrap-vue / composant HeaderNav (si pas connecté)

---
+++ frontend/src/components/HeaderNav.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        <nav class="navbar navbar-expand-lg">
+            <router-link to="/" class="navbar-brand"><img :src="logo.src" :alt="logo.alt" /></router-link>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <router-link to="/login" class="nav-link">Se connecter</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/signup" class="nav-link">S'inscrire</router-link>
+                    </li>
+                </ul>
+            </div> 
+        </nav>
+    </header>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+    name: 'HeaderNav',
+    computed: {
+        ...mapState(['logo'])
+    }
+}
+</script>
---
+++ frontend/src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    logo: {
+      src: '/images/logo.png',
+      alt: 'Logo de Groupomania'
+    }
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
